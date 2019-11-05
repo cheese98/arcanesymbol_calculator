3b0b7f78fdc6c72452e3570953a48355de356832
cheese98 <cheese98@users.noreply.github.com>
2019-11-05 21:02:31 +0900

    Update ArcaneSymbol.cpp
    모든 지역을 계산하도록 업데이트했으나 수정 필요

--- ArcaneSymbol.cpp
+++ ArcaneSymbol.cpp
@@ -46,13 +46,18 @@ public:
 	}
 };
 
-
-
 int main(){
 	Symbol sym[6];
 	int i, _level, _exp, _daily, _goal, max_date = 0;
-	long long meso_sum = 0;
-	/*
+    int time[6];
+	long long meso[6] ={0}, meso_sum = 0;
+
+    for(i=1;i<20;i++){
+		price[i]=12440000+i*6600000;
+		price0[i]=2370000+i*7130000;
+		need[i]=i*i+11;
+	}
+
 	for(i=0;i<6;i++){
 		cout << map_name[i] << endl;
 		cout << "현재 레벨 : ";
@@ -65,24 +70,15 @@ int main(){
 		cin >> _goal;
 		sym[i].setValue(_level, _exp, _daily, _goal, i);
 	}
-	*/
-	cout << map_name[0] << endl;
-	cout << "현재 레벨 : ";
-	cin >> _level;
-	cout << "경험치 : ";
-	cin >> _exp;
-	cout << "1일 획득량 : ";
-	cin >> _daily;
-	cout << "목표 : ";
-	cin >> _goal;
-	sym[0].setValue(_level, _exp, _daily, _goal, 0);
-	
-	for(i=1;i<20;i++){
-		price[i]=12440000+i*6600000;
-		price0[i]=2370000+i*7130000;
-		need[i]=i*i+11;
-	}
-	
-	cout << "업그레이드 시간 : " << sym[0].calcTime() << "일" << endl;
-	cout << "업그레이드 금액 : " << sym[0].calcMeso() << "메소" << endl;
+    for(i=0;i<6;i++){
+        time[i] = sym[i].calcTime();
+        meso[i] = sym[i].calcMeso();
+        if(time[i] > max) max = time[i];
+    }
+	for(i=0;i<6;i++){
+        cout << map_name[i] << endl;
+        cout << "업그레이드 시간 : " << time[i] << "일" << endl;
+	    cout << "업그레이드 금액 : " << meso[i] << "메소" << endl;
+    }
+
 }
